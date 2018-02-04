d19e94c0a1e049392255bcec6f659b168e41b1ba
Peter Daily <peter.daily@yahoo.co.uk>
2018-02-04 12:41:36 +0000

    refactor Reader to singleton

--- src/main/java/mooc/airport/Main.java
+++ src/main/java/mooc/airport/Main.java
@@ -3,8 +3,7 @@ package mooc.airport;
 public class Main {
 
     public static void main(String[] args) {
-	    Reader reader = new Reader(); // You could definitely make the reader a singleton too!!
-	    TextInterface ui = new TextInterface(reader);
+	    TextInterface ui = new TextInterface();
 	    ui.start();
     }
 }
